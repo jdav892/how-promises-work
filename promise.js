727214d981eb2e27d0979f28da96df1ef08b2ebc
jdav892 <jadavila9@gmail.com>
2024-06-21 18:48:17 -0400

    adding to the function

--- promise.js
+++ promise.js
@@ -1,9 +1,27 @@
 class PromiseExample {
-    constructor(executionFunction) {
+    constructor(executionFunction){
         this.promiseChain = [];
         this.handleError = () => {};
 
         this.onResolve = this.onResolve.bind(this);
         this.onReject = this.onReject.bind(this);
+        
+        executionFunction(this.onResolve, this.onReject);
+    }
+
+    then(handleSuccess){
+        this.promiseChain.push(handleSuccess);
+
+        return this;
+    }
+
+    catch(handleError){
+        this.handleError = handleError;
+
+        return this;
+    }
+
+    onResolve(value){
+        let storedValue = value;
     }
 }
