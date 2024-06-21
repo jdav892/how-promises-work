d9b497f72621645e3514f731eaf394053e9b5383
jdav892 <jadavila9@gmail.com>
2024-06-21 18:45:45 -0400

    first commit

---
+++ promise.js
@@ -0,0 +1,9 @@
+class PromiseExample {
+    constructor(executionFunction) {
+        this.promiseChain = [];
+        this.handleError = () => {};
+
+        this.onResolve = this.onResolve.bind(this);
+        this.onReject = this.onReject.bind(this);
+    }
+}
