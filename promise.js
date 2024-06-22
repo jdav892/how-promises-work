b2f6528d055ef5ba12ceec14b370fca86c2d121b
jdav892 <jadavila9@gmail.com>
2024-06-21 20:02:25 -0400

    finished, need test

--- promise.js
+++ promise.js
@@ -81,5 +81,29 @@ const makeApiCall = () => {
                 resolve(apiResponse.data);
             }
         }, 5000);
+    });
+};
+
+makeApiCall()
+    .then((user) => {
+        console.log('In the first .then()');
+
+        return user;
     })
-}
+    .then((user) => {
+        console.log(`User ${user.username}'s favorite number is ${user.favoriteNumber}`);
+        return user;
+    })
+    .then((user) => {
+        console.log(`The previous .then() told you the favoriteNumber`)
+        return user;
+    })
+    .then((user) => {
+        console.log(`The profile URL is ${profile}`);
+    })
+    .then(() => {
+        console.log('This is the last then()');
+    })
+    .catch((error) => {
+        console.error(error.message);
+    }) 
